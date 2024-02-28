8a346f469b5e99ad0f003869bddc8faa24244938
tujal.logicalstreet <tujal.logicalstreet@gmail.com>
2024-02-28 17:11:15 +0530

    change lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
-   
   end
    root "home#index"
    resources :users do
     resources :posts 
    end
-  
 end
 
 
